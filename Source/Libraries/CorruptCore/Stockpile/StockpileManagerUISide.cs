Yeprocs/RTCV

commit 16e9d7e917a103178b3a0c9ae57818580bd8f9b8

Update some message boxes to show on top

Source/Libraries/CorruptCore/Stockpile/StockpileManagerUISide.cs
@@ -466,7 +466,7 @@ namespace RTCV.CorruptCore
 
             if ((force || !File.Exists(psk.RomFilename)) && !psk.RomFilename.EndsWith("IGNORE"))
             {
-                if (DialogResult.OK == MessageBox.Show(message, title, MessageBoxButtons.OKCancel))
+                if (DialogResult.OK == MessageBox.Show(message, title, MessageBoxButtons.OKCancel, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly))
                 {
                     OpenFileDialog ofd = new OpenFileDialog
                     {
@@ -482,7 +482,7 @@ namespace RTCV.CorruptCore
                         if (Path.GetFileName(psk.RomFilename) != Path.GetFileName(filename))
                         {
                             if (DialogResult.Cancel == MessageBox.Show($"Selected file {Path.GetFileName(filename)} has a different name than the old file {Path.GetFileName(psk.RomFilename)}.\nIf you know this file is correct, you can ignore this warning.\nContinue?", title,
-                                    MessageBoxButtons.OKCancel))
+                                    MessageBoxButtons.OKCancel, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly))
                             {
                                 return false;
                             }

Source/Frontend/UI/Components/Glitch Harvester/SavestateManagerForm.cs
@@ -73,7 +73,8 @@ namespace RTCV.UI
                 if (!import && UnsavedEdits
                             && DialogResult.No == MessageBox.Show(this,
                                 "You have unsaved edits in the Glitch Harvester Savestate Manager. \n\n Are you sure you want to load without saving?",
-                                "Unsaved edits in Savestate Manager", MessageBoxButtons.YesNo))
+                                "Unsaved edits in Savestate Manager", MessageBoxButtons.YesNo, MessageBoxIcon.Warning,
+                                MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly))
                 {
                     cancelLoad = true;
                 }

Source/Frontend/UI/Modular/CoreForm.cs
@@ -88,8 +88,8 @@ namespace RTCV.UI
         {
             if (!UICore.isClosing)
             {
-                if ((S.GET<StockpileManagerForm>().UnsavedEdits && DialogResult.No == MessageBox.Show("You have unsaved edits in the Glitch Harvester Stockpile. \n\n Are you sure you want to close RTC without saving?", "Unsaved edits in Stockpile", MessageBoxButtons.YesNo))
-                 || (S.GET<SavestateManagerForm>().UnsavedEdits && DialogResult.No == MessageBox.Show("You have unsaved edits in the Glitch Harvester Savestate Manager. \n\n Are you sure you want to close RTC without saving?", "Unsaved edits in Savestate Manager", MessageBoxButtons.YesNo)))
+                if ((S.GET<StockpileManagerForm>().UnsavedEdits && DialogResult.No == MessageBox.Show("You have unsaved edits in the Glitch Harvester Stockpile. \n\n Are you sure you want to close RTC without saving?", "Unsaved edits in Stockpile", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly))
+                 || (S.GET<SavestateManagerForm>().UnsavedEdits && DialogResult.No == MessageBox.Show("You have unsaved edits in the Glitch Harvester Savestate Manager. \n\n Are you sure you want to close RTC without saving?", "Unsaved edits in Savestate Manager", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly)))
                 {
                     e.Cancel = true;
                     return;
@@ -185,7 +185,7 @@ This message only appears once.";
                         "It looks like RTC crashed or was closed improperly last time.\n" +
                         $"If you lost any unsaved work, auto-save backups may be available in {RtcCore.AutoSaveDir}.\n" +
                         "Would you like to go there now?",
-                        "RTC did not shut down cleanly", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                        "RTC did not shut down cleanly", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
                     if (showAutoSaves == DialogResult.Yes)
                     {
                         Process.Start(RtcCore.AutoSaveDir);
@@ -196,7 +196,7 @@ This message only appears once.";
                     MessageBox.Show(
                         "It looks like RTC crashed or was closed improperly last time.\n" +
                         $"If you lost any work, you can enable auto-save in the settings in case this happens again.",
-                        "RTC did not shut down cleanly", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        "RTC did not shut down cleanly", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
                 }
             }
             else

commit 46c9af3f52992adfcf908947ac01790652b95482

Fix missing arguments for MessageBox
Issue was introduced in 16e9d7e917a103178b3a0c9ae57818580bd8f9b8

Source/Libraries/CorruptCore/Stockpile/StockpileManagerUISide.cs
@@ -466,7 +466,7 @@ namespace RTCV.CorruptCore
 
             if ((force || !File.Exists(psk.RomFilename)) && !psk.RomFilename.EndsWith("IGNORE"))
             {
-                if (DialogResult.OK == MessageBox.Show(message, title, MessageBoxButtons.OKCancel, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly))
+                if (DialogResult.OK == MessageBox.Show(message, title, MessageBoxButtons.OKCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly))
                 {
                     OpenFileDialog ofd = new OpenFileDialog
                     {
@@ -482,7 +482,7 @@ namespace RTCV.CorruptCore
                         if (Path.GetFileName(psk.RomFilename) != Path.GetFileName(filename))
                         {
                             if (DialogResult.Cancel == MessageBox.Show($"Selected file {Path.GetFileName(filename)} has a different name than the old file {Path.GetFileName(psk.RomFilename)}.\nIf you know this file is correct, you can ignore this warning.\nContinue?", title,
-                                    MessageBoxButtons.OKCancel, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly))
+                                    MessageBoxButtons.OKCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly))
                             {
                                 return false;
                             }